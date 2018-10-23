0b3e89e869e245c3845a99943d7c847e86d4b7e7
N0M3N <N0M3N@users.noreply.github.com>
2018-10-23 22:18:57 +0200

    Update Stav.sql

--- SQL Model/Stav.sql
+++ SQL Model/Stav.sql
@@ -1,25 +1,5 @@
-USE [sra0051]
-GO
-
-/****** Object:  Table [dbo].[Stav]    Script Date: 22.10.2018 19:06:56 ******/
-DROP TABLE [dbo].[Stav]
-GO
-
-/****** Object:  Table [dbo].[Stav]    Script Date: 22.10.2018 19:06:56 ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE [dbo].[Stav](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[Nazev] [nvarchar](20) NOT NULL,
-PRIMARY KEY CLUSTERED 
+CREATE TABLE [dbo].[Stav]
 (
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-
-
+	[Id] [int] IDENTITY NOT NULL PRIMARY KEY,
+	[Nazev] [nvarchar](20) NOT NULL
+)
