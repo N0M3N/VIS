ae08c63f082632afbf4e31311cf3c3609f55a17c
N0M3N <N0M3N@users.noreply.github.com>
2018-10-25 19:56:25 +0200

    [Hodinova-Sazba] => [Sazba]

--- Create.sql
+++ Create.sql
@@ -67,5 +67,5 @@ CREATE TABLE [dbo].[Mzdy]
 	[Id] [int] IDENTITY NOT NULL PRIMARY KEY,
 	[Uzivatel-Id] [int] NOT NULL FOREIGN KEY REFERENCES [dbo].[Uzivatel] ([Id]),
 	[Zakazka-Id] [int] NOT NULL FOREIGN KEY REFERENCES [dbo].[Zakazka] ([Id]),
-	[Hodinova-Sazba] [int] NOT NULL
+	[Sazba] [int] NOT NULL
 )
--- Databse/MzdaEntity.cs
+++ Databse/MzdaEntity.cs
@@ -6,13 +6,13 @@ namespace Databse
 {
     internal class MzdaEntity : Entity<MzdaModel>
     {
-        protected override string SQL_SELECT => "SELECT [Id], [Uzivatel-Id], [Zakazka-Id], [Hodinova-Sazba] FROM [dbo].[Mzdy];";
+        protected override string SQL_SELECT => "SELECT [Id], [Uzivatel-Id], [Zakazka-Id], [Sazba] FROM [dbo].[Mzdy];";
 
-        protected override string SQL_SELECT_ID => "SELECT [Id], [Uzivatel-Id], [Zakazka-Id], [Hodinova-Sazba] FROM [dbo].[Mzdy] WHERE [Id] = @p_id;";
+        protected override string SQL_SELECT_ID => "SELECT [Id], [Uzivatel-Id], [Zakazka-Id], [Sazba] FROM [dbo].[Mzdy] WHERE [Id] = @p_id;";
 
-        protected override string SQL_INSERT => "INSERT INTO[dbo].[Mzdy]([Uzivatel-Id], [Zakazka-Id], [Hodinova-Sazba]) VALUES(@p_uzivatelId, @p_zakazkaId, @p_sazba);";
+        protected override string SQL_INSERT => "INSERT INTO[dbo].[Mzdy]([Uzivatel-Id], [Zakazka-Id], [Sazba]) VALUES(@p_uzivatelId, @p_zakazkaId, @p_sazba);";
 
-        protected override string SQL_UPDATE => "UPDATE [dbo].[Mzdy] SET [Uzivatel-Id] = @p_uzivatelId, [Zakazka-Id] = @p_zakazkaId, [Hodinova-Sazba] = @p_sazba WHERE [Id] = @p_id;";
+        protected override string SQL_UPDATE => "UPDATE [dbo].[Mzdy] SET [Uzivatel-Id] = @p_uzivatelId, [Zakazka-Id] = @p_zakazkaId, [Sazba] = @p_sazba WHERE [Id] = @p_id;";
 
         protected override string SQL_DELETE => "DELETE FROM [dbo].[Mzdy] WHERE [Id] = @p_id;";
 
